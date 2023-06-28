66c7b118f392764db719ca2a37c5daeff610969a
datogithub <104518080+datogithub@users.noreply.github.com>
2023-06-28 14:08:57 -0700

    axios: import rapidAPI (youtube v3)

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,28 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 import Sidebar from './Sidebar';
 import Videos from './Videos';
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2} }}>
-        <Sidebar /> 
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        /> 
 
 
         <Typography className='copyright'
@@ -24,10 +38,10 @@ const Feed = () => {
                     mb={2}
                     sx={{ color: "white" }}
         >
-          New <span style={{ color: "#F31503"}}>videos</span>
+          {selectedCategory} <span style={{ color: "#F31503"}}>videos</span>
         </Typography>
 
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
     method: 'GET',
-    url: BASE_URL,
     params: {
       maxResults: '50'
     },
@@ -14,3 +13,9 @@ const options = {
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
+
+  export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+  }
