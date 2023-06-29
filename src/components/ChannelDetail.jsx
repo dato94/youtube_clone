7601f4d1063a8b19a35245d91e311bb24af1cf11
datogithub <104518080+datogithub@users.noreply.github.com>
2023-06-29 15:45:46 -0700

    create channelDetail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { videos } from "./";
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+
+  const [ChannelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=data`)
+    .then((data) => setVideos(data?.items));
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <div>{id}</div>
   )
 }
 
