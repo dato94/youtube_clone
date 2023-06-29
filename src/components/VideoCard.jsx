c86e1c53dc891b560e17ce1aef39d25946f6c428
datogithub <104518080+datogithub@users.noreply.github.com>
2023-06-29 15:13:52 -0700

    create videoCard url video

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -11,7 +11,7 @@ import { demoThumbnailUrl,
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
     
   return (
-    <Card>
+    <Card sx={{ width: { md: "340px", xs: "100%" }, boxShadow: "none", borderRadius: 0 }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
             <CardMedia 
                 image={snippet?.thumbnails?.high?.url} 
@@ -19,6 +19,23 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
                 sx={{ width: 358, height: 180}}
             />
         </Link>
+        <CardContent sx={{ background: "#1e1e1e", height: "106px"}}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <Typography variant="subtitle1"
+                        fontWeight="bold"
+                        color="#FFF">
+                {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+            </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+            <Typography variant="subtitle2"
+                        fontWeight="bold"
+                        color="grey">
+                {snippet?.channelTitle || demoChannelTitle}
+                <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px"}} />
+            </Typography>
+        </Link>
+        </CardContent>
     </Card>
   )
 }
